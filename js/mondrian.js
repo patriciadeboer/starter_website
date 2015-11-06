32eb2e99f5c641fd34ff6f776eb73cb711419e96
Patricia de Boer <pdeboer05@gmail.com>
2015-11-06 09:47:18 -0600

    included clear canvas. Next will try to add change palette

--- js/mondrian.js
+++ js/mondrian.js
@@ -3,6 +3,24 @@
 
 $(document).ready(function () {
 
+	//Clear canvas
+	$('.clearCanvas').on('click',clearCanvas);
+
+	function clearCanvas() {
+		//$(this).css("background-color", "white")
+		$('.boxes').css("background-color", "white")
+		$('.masterpiece').css("visibility", "hidden")
+	};
+
+	//Change color pallete
+	function changePallete(){
+		var color1= 'green';
+		var color2= 'pink';
+		var color3= 'red';
+		var original = ['yellow', 'orange','blue']
+	}
+
+	//selecting and applying the color
 	var selectedColor;
 
 	$('.activeColor').on('click', findColor);
@@ -16,6 +34,9 @@ $(document).ready(function () {
 	function changeColor() {
 
 		$(this).css("background-color", selectedColor );
+		$('.masterpiece').css("visibility", "visible")
 	};
+
+
 
 });
